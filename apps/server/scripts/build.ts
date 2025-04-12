7f653358ecdafe5dda80a2c40be962c4c8b142fb
firesquid6 <jdeiss06@gmail.com>
2025-04-12 17:49:16 -0500

    add a proper build function to the server

---
+++ apps/server/scripts/build.ts
@@ -0,0 +1,25 @@
+import path from "path";
+
+async function main() {
+
+  const buildDir = path.resolve(__dirname, "../build");
+  const entrypoint = path.resolve(__dirname, "../src/index.ts");
+
+  const result = await Bun.build({
+    entrypoints: [entrypoint],
+    outdir: buildDir,
+    minify: true,
+  });
+
+
+  if (!result.success) {
+    console.log("Build failed:");
+    console.log(result);
+    return;
+  }
+
+  console.log("Build succeeded!");
+}
+
+
+main();
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,22 +1,4 @@
-import { Command } from "commander";
-
-const program = new Command();
-
-program
-  .command("test-commit")
-  .description("Runs a test commit to ensure that everything is working properly")
-  .action(() => {
-    console.log("running an example commit!");
-  });
-
-program
-  .command("start")
-  .description("Starts the server")
-  .action(() => {
-    console.log("starting the server!");
-  });
+import { runCli } from "@/cli";
 
 
-export async function runCli() {
-  await program.parseAsync();
-}
+runCli();
