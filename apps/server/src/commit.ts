9216993930914fec03f8f0e9a0d136762d89d62c
firesquid6 <jdeiss06@gmail.com>
2025-04-14 12:26:44 -0500

    add function to generate a tree from a list of filepaths

--- apps/server/src/commit.ts
+++ apps/server/src/commit.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-export async function newBlob(filepath: string, blobsDirectory: string) {
+export async function getBlobPath(filepath: string, blobsDirectory: string): Promise<string> {
   const hasher = new Bun.CryptoHasher("sha256");
   const file = Bun.file(filepath);
 
@@ -17,21 +17,35 @@ export async function newBlob(filepath: string, blobsDirectory: string) {
   let collisionIndex = 0;
   let blobPath = path.join(blobsDirectory, `blobs-${collisionIndex}`, hash);
   
-  while (!fs.existsSync(blobPath)) {
+  while (fs.existsSync(blobPath)) {
+    const text = fs.readFileSync(blobPath);
+
+    // we already have this file compressed. Don't need to keep looking
+    if (text === compressed) {
+      return blobPath;
+    }
+
     collisionIndex += 1;
     blobPath = path.join(blobsDirectory, `blobs-${collisionIndex}`, hash);
   }
 
   fs.mkdirSync(path.dirname(blobPath), { recursive: true });
   fs.writeFileSync(blobPath, compressed);
+
+  return blobPath;
 }
 
-export async function makeNewTree() {
-
+interface TreeNode {
+  filepath: string;
+  blobPath: string;
 }
 
-
-
-export function hasExistingBlob(filepath: string, blobsDirectory: string) {
-  
+export async function makeNewTree(filepaths: string[], blobsDirectory: string): Promise<TreeNode[]> {
+  return Promise.all(filepaths.map(async (filepath): Promise<TreeNode> => {
+    return {
+      filepath,
+      blobPath: await getBlobPath(filepath, blobsDirectory),
+    }
+  }));
 }
+
