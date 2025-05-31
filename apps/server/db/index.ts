0f4d711f2907849afa9decd7ff45892aa9d1a1bc
firesquid6 <jdeiss06@gmail.com>
2025-05-31 18:02:40 -0500

    add drizzle config

--- apps/server/db/index.ts
+++ apps/server/db/index.ts
@@ -2,11 +2,18 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import path from "path";
 import type { Config } from "../config";
 import { Database } from "bun:sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 export type Db = ReturnType<typeof getDb>;
 
 export function getDb(config: Config) {
   const sqlite = new Database(path.join(config.storeDirectory, "database.sqlite"));
   const db = drizzle({ client: sqlite });
+  console.log("Migrating database...");
+  migrate(db, {
+    migrationsFolder: "drizzle",
+    migrationsSchema: "db/schema.ts",
+  });
+  console.log("Database migrated!");
   return db;
 }
