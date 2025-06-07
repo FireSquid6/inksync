a41c18eeaa34d151dd9cfb5de6082dcc2baf9506
firesquid6 <jdeiss06@gmail.com>
2025-06-07 13:32:46 -0500

    simplify logger

--- apps/server/http/logger.ts
+++ apps/server/http/logger.ts
@@ -1,25 +1,7 @@
 import Elysia from "elysia";
 
 function logResponse(method: string, path: string, code: number | string) {
-  let marker = "+";
-
-  while (method.length < 5) {
-    method = method + " ";
-  }
-
-  if (typeof code === "number" && code >= 400) {
-    if (code < 500) {
-      marker = "-";
-    } else {
-      marker = "!";
-    }
-  } else {
-    if (code === "INTERNAL_SERVER_ERROR" || code === "UNKNOWN" || code === "INVALID_COOKIE_SIGNATURE") {
-      marker = "!";
-    } 
-  }
-
-  console.log(`${marker} ${method} ${path} -> ${code}`);
+  console.log(`- ${method} ${path} -> ${code}`);
 }
 
 export const loggerPlugin = new Elysia()
