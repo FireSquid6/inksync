fae47e684b6e3325f67422d3656447228ce8ffbf
firesquid6 <jdeiss06@gmail.com>
2025-06-07 21:03:26 -0500

    add log for config path

--- apps/server/main.ts
+++ apps/server/main.ts
@@ -4,5 +4,6 @@ import { getConfigFromFile } from "./config";
 const configPath = process.env.INKSYNC_CONFIG_PATH ?? "./inksync-server-config.yaml";
 const config = getConfigFromFile(configPath);
 
+console.log(`Using config path ${configPath}`)
 
 startApp(config);
