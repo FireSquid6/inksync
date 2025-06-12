fc180db035c915fc9249c62e644a9062a6433b73
firesquid6 <jdeiss06@gmail.com>
2025-06-11 23:56:09 -0500

    add more to the mobile app

---
+++ packages/mobile/android/app/src/main/java/com/inksync/dev/MainActivity.java
@@ -0,0 +1,5 @@
+package com.inksync.dev;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ packages/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.inksync.dev',
+  appName: 'Inksync Mobile',
+  webDir: 'dist'
+};
+
+export default config;
