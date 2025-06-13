306637436739efe2edb60cc119c5489eb9395a1e
firesquid6 <jdeiss06@gmail.com>
2025-06-13 13:34:47 -0500

    add a script to push docker

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+VERSION=$1
+IMAGE_NAME=firesquid/inksync:"$VERSION"
 
-echo "Buidling the dockerfile"
-docker build -t firesquid/inksync .
+if [ -z "$1" ]; then
+  echo "No version provided."
+  exit 1
+fi
+
+echo "Buidling the image:"
+docker build -t "$IMAGE_NAME" .
+
+echo "Pusing the image:"
+docker push "$IMAGE_NAME"
+
